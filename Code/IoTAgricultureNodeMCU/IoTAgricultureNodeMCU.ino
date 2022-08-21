92f7e8d849dc9ae25bb2093f5f2c7f33c17e7b95
Prateek <prateek.ideafires@gmail.com>
2022-08-21 15:51:59 +0530

    Adding First Version Of Code

---
+++ Code/IoTAgricultureNodeMCU/IoTAgricultureNodeMCU.ino
@@ -0,0 +1,114 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+//#include <ThingSpeak.h>
+
+#define SoilMoisturePin A0  // used for Arduino and ESP8266
+#define RelayPin 4  // Relay Pin
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+
+String writeApiKey = "ZD9H28GE89NGE5W5"; // Enter your Write API key from ThingSpeak
+String readApiKey = "G49I8ZULFYU75XH9"; // Enter your Read API key from ThingSpeak
+unsigned long myChannelNumber = 1837837;
+float humidity = 0.0;
+float temperature = 0.0;
+float soilMoisture = 0.0;
+bool pumpStatus=false;//False==OFF,True==ON
+
+const char *ssid = "Humphrey"; // replace with your wifi ssid and wpa2 key
+const char *pass = "bluelight";
+const char* iotServer = "api.thingspeak.com";
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  pinMode(RelayPin,OUTPUT);
+  dht.begin();
+  delay(10);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  //ThingSpeak.begin(client);
+}
+
+void loop() 
+{
+  delay(2000);
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  soilMoisture = analogRead(SoilMoisturePin);
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    humidity=0;
+    temperature=0;
+    //return;
+  }
+  Serial.print("Humidity:");
+  Serial.println(humidity);
+  Serial.print("Temperature:");
+  Serial.println(temperature);
+  Serial.print("SoilMoisture:");
+  Serial.println(soilMoisture);
+  
+  if(soilMoisture<800)
+  {
+    digitalWrite(RelayPin, LOW); 
+    pumpStatus=false;
+  }
+  else
+  {
+   digitalWrite(RelayPin, HIGH); 
+   pumpStatus=true;
+  }
+  sendToIotServer();
+  delay(30000);
+} 
+
+void sendToIotServer()
+{
+  if (client.connect(iotServer,80)>0) // “184.106.153.149” or api.thingspeak.com
+  {
+    Serial.println("ThingSpeak Connected");
+
+    String postStr = writeApiKey;
+    postStr +="&field1=";//SoilMoisture
+    postStr += String(soilMoisture);
+    postStr +="&field2=";//Humidity
+    postStr += String(humidity);
+    postStr +="&field3=";//Temperature
+    postStr += String(temperature);
+    postStr +="&field4=";//PumpStatus
+    postStr += String(pumpStatus);
+    postStr += "\r\n\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+writeApiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+  
+    Serial.println("Post String:");
+    Serial.println( postStr) ;
+   
+    Serial.println("Sent to Thingspeak.");
+  
+  }
+  client.stop();
+
+}
